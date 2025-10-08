5d99ae9f4eb620d17aa6154678ba54e8bf9a3eff
m-nikolovska-mak-system <m.nikolovska@mak-system.net>
2025-10-08 14:33:36 +0200

    Second commit

--- testGradle/app/src/main/java/org/example/Main.java
+++ testGradle/app/src/main/java/org/example/Main.java
@@ -5,15 +5,16 @@ import java.time.LocalDate;
 import model.BloodBank;
 import model.Donation;
 import model.Donor;
+import repository.DonorRepository;
+import service.BloodDonationService;
 
 public class Main {
     public static void main(String[] args) {
-        Donor donor = new Donor(1, "Ana Petrova", "O+", LocalDate.of(1995, 5, 14), "071234567");
-        Donation donation = new Donation(1, donor, LocalDate.now(), 0.45);
-        BloodBank bank = new BloodBank("Red Cross Skopje", "Skopje");
-        bank.addDonation(donation);
+    	  DonorRepository repo = new DonorRepository();
+          BloodDonationService service = new BloodDonationService(repo);
 
-        System.out.println(bank);
-        System.out.println(donation);
+          service.registerDonor("Elena Petrova", "A+");
+          service.registerDonor("Milan Jovanov", "O-");
+          service.listDonors();
     }
 }
